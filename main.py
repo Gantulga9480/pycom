9914f0b6711f8c7eb1b77412bc3083222c9ae640
Gantulga-ASUS-Ubuntu <limited.tulgaa@gmail.com>
2021-01-16 19:38:30 +0800

    test

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 # Github Gantulga1234
-from network import WLAN 
-from mqtt import MQTTClient 
-import machine 
-import gc
+from network import WLAN
+from mqtt import MQTTClient
+import machine
 import time
 import pycom
 from lib.GridEye import GridEye
 
 SENSOR_START = False
 TRY = True
-SENSOR = 2
+SENSOR = 5
 
 def sub_cb(topic, msg):
     global SENSOR_START, TRY
@@ -37,10 +36,7 @@ wlan = WLAN(mode=WLAN.STA)
 wlan.connect("Univision_83A3", auth=(WLAN.WPA2, "88640783")) 
 while not wlan.isconnected():  
     machine.idle()
-time.sleep(0.1)
-pycom.rgbled(0x00007D)
-time.sleep(0.1)
- 
+
 # client connection
 client = MQTTClient("wipy{}".format(SENSOR), "192.168.1.2", port=1883) 
 client.set_callback(sub_cb)
@@ -53,6 +49,19 @@ client.subscribe(topic="wipy/sensor-start", qos=0)
 time.sleep(0.1)
 # using GridEye to get readings
 ge = GridEye()
+time.sleep(5)
+
+int_table= ge.get_interrupts(reset=True)
+
+time.sleep(5)
+ge.get_states()
+ge.get_interrupts(reset=True)
+time.sleep(5)
+ge.get_interrupts(reset=True)
+ge.get_states()
+
+# return a 8x8 matrix + min&max heats out of them
+image = ge.get_sensor_data()
 ge.reset(flags_only=True)
 time.sleep(0.1)
 
@@ -62,7 +71,10 @@ while TRY:
     if SENSOR_START:
         count = count + 1
         # return a 8x8 matrix + min&max heats out of them
-        image = ge.get_sensor_data("GRAYIMAGE")
+        try:
+            image = ge.get_sensor_data()
+        except:
+            client.publish("sensors/sensor{}/status".format(SENSOR), "d")
         image_data=str(image[0])
         if (count>2):
             if count == 23:
@@ -70,7 +82,6 @@ while TRY:
                 count = 3
             # publish image_data to "sensors/sensor1" topic
             client.publish("sensors/sensor{}/data".format(SENSOR), image_data)
-            time.sleep(0.05)
         else:
             client.publish("sensors/sensor{}/status".format(SENSOR), "c")
             time.sleep(0.1)
